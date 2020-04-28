ebc867e8371edc655cd5c41137dc707899bfe211
David Bakaleinik <david.bakaleinik@mymail.champlain.edu>
2020-04-28 11:10:28 -0400

    It builds again, hallelujah it builds again!

--- FrostPainterV3/Source/FrostPainterV3Editor/FrostPainterV3Editor.Build.cs
+++ FrostPainterV3/Source/FrostPainterV3Editor/FrostPainterV3Editor.Build.cs
@@ -5,7 +5,7 @@ public class FrostPainterV3Editor : ModuleRules
     public FrostPainterV3Editor(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Slate", "SlateCore" });
 
         PublicDependencyModuleNames.AddRange(new string[] { "FrostPainterV3Editor" });
 
